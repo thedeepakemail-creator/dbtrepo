6427bacda82a17e42b300a5feaa9bcf55d7f5256
thedeepakemail-creator <thedeepakemail@gmail.com>
2026-02-14 07:11:37 +0530

    Create sample_proc.sql

---
+++ tests/sample_proc.sql
@@ -0,0 +1,20 @@
+create or replace procedure pr_cal_gratuity (p_empno number)
+returns varchar  
+language sql
+as 
+$$  
+declare  
+    v_name varchar;
+    v_Sal number;
+    v_gratuity number;
+begin
+
+ select  ename,sal,(sal * datediff('years',hiredate,dor) * 15) / 26 as gratuity 
+ into v_name,v_Sal,v_gratuity
+ from emp where empno=:p_empno;
+
+ update emp set gratuity=:v_gratuity where empno=:p_empno;
+ 
+  return 'Gratuty updated for '||v_name;
+end;
+$$;	
